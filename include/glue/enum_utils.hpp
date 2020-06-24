9877f7e3262999e53d6e41c5e0b4e7f2e4e02d7e
Amy Parent <amy@amyparent.com>
2020-06-24 12:17:32 +0100

    Fix storage class stuff

--- include/glue/enum_utils.hpp
+++ include/glue/enum_utils.hpp
@@ -12,7 +12,7 @@
 #include <cstdint>
 
 namespace amyinorbit::gl {
-    template <typename T> constexpr GLenum as_enum();
+    template <typename T> static constexpr GLenum as_enum();
 
     template <> constexpr GLenum as_enum<float>() { return GL_FLOAT; }
     template <> constexpr GLenum as_enum<int>() { return GL_INT; }
@@ -20,8 +20,8 @@ namespace amyinorbit::gl {
     template <> constexpr GLenum as_enum<double>() { return GL_DOUBLE; }
     template <> constexpr GLenum as_enum<std::uint8_t>() { return GL_UNSIGNED_BYTE; }
 
-    template <int D> constexpr GLenum tex_enum = GL_NONE;
-    template <> constexpr GLenum tex_enum<1> = GL_TEXTURE_1D;
-    template <> constexpr GLenum tex_enum<2> = GL_TEXTURE_2D;
-    template <> constexpr GLenum tex_enum<3> = GL_TEXTURE_3D;
+    template <int D> static constexpr GLenum tex_enum = GL_NONE;
+    template <> static constexpr GLenum tex_enum<1> = GL_TEXTURE_1D;
+    template <> static constexpr GLenum tex_enum<2> = GL_TEXTURE_2D;
+    template <> static constexpr GLenum tex_enum<3> = GL_TEXTURE_3D;
 }
--- include/glue/gc.hpp
+++ include/glue/gc.hpp
@@ -12,6 +12,7 @@
 #include <type_traits>
 #include <unordered_map>
 #include <cassert>
+#include <string>
 
 // #define GL_GC_TRACE
 #ifdef GL_GC_TRACE
